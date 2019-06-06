ea03c133fe93b25f1fa39e106cff310c3188a12a
Edwaldo Almeida <contact@edwaldoalmeida.com>
2019-06-06 11:53:37 -0400

    minor adjusts

--- 02.sumTwoSmallestNumbers.js
+++ 02.sumTwoSmallestNumbers.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+"use strict";
 
 /// Problem statement (codewars.com):
 // Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 integers. No floats or empty arrays will be passed.
@@ -20,57 +21,57 @@
 /// Solution code V1:
 function sumTwoSmallestNumbersV1 (arr){
     
-    var lowestOne = arr[0]
-    var lowestTwo = arr[1]
+    var lowestOne = arr[0];
+    var lowestTwo = arr[1];
     
     for (let i = 2; i < arr.length; i++){
         
-        let beingCompared = arr[i]
+        let beingCompared = arr[i];
         
         if ((beingCompared - lowestOne < 0) && (beingCompared - lowestOne < beingCompared - lowestTwo)){
-            lowestOne = beingCompared
+            lowestOne = beingCompared;
         } else if ((beingCompared - lowestTwo < 0) && (beingCompared - lowestTwo < beingCompared - lowestOne)){
-            lowestTwo = beingCompared
+            lowestTwo = beingCompared;
         }
     }
     
-    return lowestOne + lowestTwo
+    return lowestOne + lowestTwo;
     
 }
 
 /// Solution code V2:
 function sumTwoSmallestNumbersV2 (arr){
 
-    var lowestOne = arr[0]
-    var lowestTwo = arr[1]
+    var lowestOne = arr[0];
+    var lowestTwo = arr[1];
     
     for (let i = 2; i < arr.length; i++){
         
-        let numberBeingCompared = arr[i]
+        let numberBeingCompared = arr[i];
 
-        let numberBeingComparedIsLesserThanLowestOne = numberBeingCompared - lowestOne < 0
-        let numberBeingComparedIsLesserThanLowestTwo = numberBeingCompared - lowestTwo < 0
+        let numberBeingComparedIsLesserThanLowestOne = numberBeingCompared - lowestOne < 0;
+        let numberBeingComparedIsLesserThanLowestTwo = numberBeingCompared - lowestTwo < 0;
 
-        let diffNumberBeingComparedAndLowestOne = numberBeingCompared - lowestOne
-        let diffNumberBeingComparedAndLowestTwo = numberBeingCompared - lowestTwo
+        let diffNumberBeingComparedAndLowestOne = numberBeingCompared - lowestOne;
+        let diffNumberBeingComparedAndLowestTwo = numberBeingCompared - lowestTwo;
 
         if ((numberBeingComparedIsLesserThanLowestOne) && (diffNumberBeingComparedAndLowestOne < diffNumberBeingComparedAndLowestTwo)){
-            lowestOne = numberBeingCompared
+            lowestOne = numberBeingCompared;
         } else if ((numberBeingComparedIsLesserThanLowestTwo) && (diffNumberBeingComparedAndLowestTwo < diffNumberBeingComparedAndLowestOne)){
-            lowestTwo = numberBeingCompared
+            lowestTwo = numberBeingCompared;
         }
     }
 
-    return lowestOne + lowestTwo
+    return lowestOne + lowestTwo;
 
 }
 
 // Testing V1:
-console.log(sumTwoSmallestNumbersV1([19, 5, 42, 2, 77]))
-console.log(sumTwoSmallestNumbersV1([1, 5, 42, 2, 77]))
-console.log(sumTwoSmallestNumbersV1([10, 20, 42, 30, 77, 155, 1333, 2422, 35353, 353343, 2, 12313]))
+console.log(sumTwoSmallestNumbersV1([19, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbersV1([1, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbersV1([10, 20, 42, 30, 77, 155, 1333, 2422, 35353, 353343, 2, 12313]));
 
 // Testing V2:
-console.log(sumTwoSmallestNumbersV2([19, 5, 42, 2, 77]))
-console.log(sumTwoSmallestNumbersV2([1, 5, 42, 2, 77]))
-console.log(sumTwoSmallestNumbersV2([10, 20, 42, 30, 77, 155, 1333, 2422, 35353, 353343, 2, 12313]))
+console.log(sumTwoSmallestNumbersV2([19, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbersV2([1, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbersV2([10, 20, 42, 30, 77, 155, 1333, 2422, 35353, 353343, 2, 12313]));
--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-# Coding Exercises - JavaScript
+# Coding Exercises in JavaScript
 
-These are coding exercises I've been doing for improving my coding skills and to keep the wheel moving when it comes to times of not so much coding activity.
+These are coding exercises I've been doing for improving my coding skills and to keep the wheel turning.
 
 ## Running
 
-I've developed these files using macOS and Node.
+I've developed these files using macOS, Node and VS Code.
 
-You can run them by either doing `./filename.js` or `node filename.js`
+You can run them by doing
+
+1. `./filename.js`
+2. `node filename.js`
+3. running from inside VS Code (see below)
+
+### Setting VS Code for debugging and running (my preferred)
+
+1. Activate Node debugging in `Code` / `Preferences` / `Settings`
+   1. search for `node debug`
+   2. set `Debug > Node: Auto Attach` to `on`
+2. Set VS Code to run the scripts in `Debug` / `Open Configurations`
+   1. select `Node.js`
+   2. it will create a folder `.vscode` and add a `launch.json`
+
+Now, it's possible to run the scripts from inside VS Code, debugging (F5) or not (^F5)
