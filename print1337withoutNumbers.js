7dd9192d1a9a7b3e1dd499cb8d0ed285997e64e4
Edwaldo Almeida <contact@edwaldoalmeida.com>
2019-06-15 06:59:40 -0400

    updating challenge description and using 'lets'

--- print1337withoutNumbers.js
+++ print1337withoutNumbers.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 "use strict";
+
 /**
  * Challenge:
- * print the number 1337 without using numbers
+ * Your task is to make a program that prints the number 1337,
+ * without using any of the characters 0123456789 in your code,
+ * and independently of any external variables such as the date or time
+ * or a random seed.
+ * We want you to create the shortest code (counting in bytes) possible.
  */
-// solution
-one = 'a';
-three = 'aaa';
-seven = 'aaaaaaa';
-n = '';
+
+// Solution:
+let one = 'a';
+let three = 'aaa';
+let seven = 'aaaaaaa';
+let n = '';
 n += one.length;
 n += three.length;
 n += three.length;
